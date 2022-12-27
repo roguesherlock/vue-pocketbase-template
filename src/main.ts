f9c6d9d975b22a3326fd2659770cab734d51938d
Akash <aakash@hey.com>
2022-12-27 21:57:04 +0530

    add tanstack vue query

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,39 @@
 import { createApp } from "vue"
 import { createPinia } from "pinia"
-
-import App from "./App.vue"
-import router from "./router"
+import { VueQueryPlugin } from "@tanstack/vue-query"
+import { persistQueryClient } from "@tanstack/query-persist-client-core"
+import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister"
+import type { VueQueryPluginOptions } from "@tanstack/vue-query"
+import App from "@/App.vue"
+import router from "@/router"
 import { pb } from "@/lib"
 
 import "./assets/main.css"
 import "@/assets/tailwind.css"
 import { pocketBaseSymbol } from "@/symbols/injectionSymbols"
 
+const vueQueryOptions: VueQueryPluginOptions = {
+  queryClientConfig: {
+    defaultOptions: {
+      queries: {
+        cacheTime: 1000 * 60 * 60 * 24, // 24 hours
+        staleTime: 1000 * 60 * 5, // 5 minutes
+      },
+    },
+  },
+  clientPersister: (queryClient) => {
+    return persistQueryClient({
+      queryClient,
+      persister: createSyncStoragePersister({ storage: localStorage }),
+    })
+  },
+}
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(VueQueryPlugin, vueQueryOptions)
 app.provide(pocketBaseSymbol, pb)
 
 app.mount("#app")
