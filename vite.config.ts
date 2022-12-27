7c56338f9bb0b1144b1d32a1ce58fef141c016eb
Akash <aakash@hey.com>
2022-12-27 22:12:23 +0530

    make it pwa!

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,45 @@ import { fileURLToPath, URL } from "node:url"
 
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
+import { VitePWA } from "vite-plugin-pwa"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        maximumFileSizeToCacheInBytes: 30000000, // ~30Mib
+        globPatterns: ["**/*.{js,css,html,ico}"],
+      },
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+      manifest: {
+        name: "YOUR_APP_NAME",
+        short_name: "YOUR_APP_SHORT_NAME",
+        description: "YOUR_APP_DESCRIPTION",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "favicons/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "favicons/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "favicons/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
